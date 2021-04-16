fb15d7305fc5fb774f32a826a26f515d8fd529bf
Maksim Gavrilov <gavrilovmaksim91@gmail.com>
2021-04-16 11:07:11 +0300

    увше руфв

--- components/HeadVideo.vue
+++ components/HeadVideo.vue
@@ -16,7 +16,7 @@
       <div class="row h-100 align-items-center">
         <div class="col-lg-12">
           <h1 class="display-4 text-white mt-5 mb-2">
-            Заводской ремонт турбокомпрессоров
+            Заводской ремонт турбин
           </h1>
           <p class="lead mb-5 text-white">
             Если турбина дуть больше не может, отправьте заявку, и мы Вам
